061a8188c9a5afa533feed1fe792abec10becf07
Nathan <nathanfpedroza@gmail.com>
2019-08-26 09:02:53 -0300

    asfsafasfd

--- roteiro3-118110342.sql
+++ roteiro3-118110342.sql
@@ -1,26 +1,38 @@
 
 
 CREATE TABLE farmacia {
-    id BIGINT UNIQUE,
-    sede BOOLEAN,
-    endereco TEXT,
-    gerenteCpf VARCHAR(11)
+    id          BIGINT UNIQUE,
+    sede        BOOLEAN,
+    endereco    TEXT,
+    gerenteCpf  VARCHAR(11)
 
 };
 
 CREATE TABLE funcionario {
-    nome TEXT NOT NULL,
-    cpf VARCHAR(11) PRIMARY KEY,
-    cargo VARCHAR(20),
-    farmaciaId BIGINT REFERENCES farmacia(id),
+    nome        TEXT NOT NULL,
+    cpf         CHAR(11) PRIMARY KEY,
+    cargo       VARCHAR(20),
+    farmaciaId  BIGINT REFERENCES farmacia(id),
 
     CONSTRAINT cargos CHECK (cargo IN ( 'farmaceutico', 'vendedor', 'entregador',
-    'caixa', 'administrador', 'gerente'))
+    'caixa', 'administrador', 'gerente', null))
     
 };
 
 CREATE TABLE medicamento {
-    nome TEXT,
-    requerReceita BOOLEAN,
+    nome        TEXT,
+    receita     BOOLEAN,
 
 };
+
+CREATE TABLE cliente {
+    nome        TEXT,
+    cpf         CHAR(11),
+    enderecos      
+}
+
+CREATE TABLE enderecos {
+    cpfCliente  CHAR(11) REFERENCES cliente(cpf),
+    endereco1   TEXT,
+    
+}
