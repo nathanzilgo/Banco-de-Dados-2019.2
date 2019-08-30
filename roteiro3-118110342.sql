e28b8a767232756f2b0ca8ee4d5ea25fb5cb5a73
nathan <nathanfpedroza@gmail.com>
2019-08-30 20:50:57 -0300

    rajada pai

--- roteiro3-118110342.sql
+++ roteiro3-118110342.sql
@@ -36,7 +36,6 @@ CREATE TABLE medicamento (
     id          BIGINT PRIMARY KEY,
     nome        TEXT NOT NULL,
     receita     BOOLEAN NOT NULL,
-    bula        TEXT,
     fabricante  TEXT
     
 );
@@ -68,20 +67,22 @@ CREATE TABLE venda (
     funcionarioCpf      CHAR(11) REFERENCES funcionario(cpf) NOT NULL,
     funcionarioCargo    cargoType NOT NULL,
     clienteCpf          CHAR(11) REFERENCES cliente(cpf) ON DELETE RESTRICT,
-    clienteEnd          CHAR(11) REFERENCES enderecosCliente(id),
+    clienteTemEnd       BOOLEAN NOT NULL,
     medId               BIGINT REFERENCES medicamento(id) ON DELETE RESTRICT,
     medReceita          BOOLEAN NOT NULL,
 
-    CONSTRAINT clienteOuNao CHECK(clienteCpf = NULL AND clienteEnd = NULL OR clienteCpf != NULL AND clienteEnd != NULL),
+    CONSTRAINT clienteOuNao CHECK(clienteCpf = NULL AND clienteTemEnd = FALSE OR clienteCpf != NULL AND clienteTemEnd = TRUE),
     CONSTRAINT receitaReq   CHECK(medReceita = TRUE AND clienteCpf != NULL OR medReceita = FALSE),
     CONSTRAINT funcionario  CHECK(funcionarioCargo = 'vendedor')
     
 );
 
 CREATE TABLE entrega (
-    clienteCpf  CHAR(11) REFERENCES cliente(cpf),
-    medId       BIGINT REFERENCES medicamento(id),
-    clienteEnd  CHAR(11) REFERENCES enderecosCliente(id)
+    clienteCpf      CHAR(11) REFERENCES cliente(cpf),
+    medId           BIGINT REFERENCES medicamento(id),
+    clienteTemEnd   BOOLEAN NOT NULL,
+
+    CONSTRAINT clienteTemEndereco CHECK(clienteTemEnd = TRUE)
 );
 
 ALTER TABLE farmacia ADD CONSTRAINT tipos CHECK(tipo IN ('sede', 'filial'));
@@ -92,12 +93,26 @@ ALTER TABLE farmacia ADD CONSTRAINT tipos CHECK(tipo IN ('sede', 'filial'));
 INSERT INTO farmacia VALUES(0, 'farmacia dias', 'filial', 'alguma rua', 'algum bairro', 
 158, 'paraiba', '56734523410');
 INSERT INTO funcionario VALUES('alfredo', '12345678923', 'vendedor', FALSE, 0);
-INSERT INTO funcionario VALUES('Alan', '12345678543', 'administrador', TRUE, 0);
+INSERT INTO funcionario VALUES('Patricia', '12345678543', 'administrador', TRUE, 0);
 
 INSERT INTO farmacia VALUES(1, 'drogasil', 'sede', 'jose de alguma coisa', 'centro', 
 451, 'paraiba', '32345678543');
+
 INSERT INTO funcionario VALUES('Jorge', '32345678543', 'farmaceutico', TRUE, 1);
+
+INSERT INTO medicamento VALUES(1, 'alprazolam', TRUE, 'ems');
+INSERT INTO medicamento VALUES(30, 'neosoro', FALSE, 'n sei');
+
+INSERT INTO cliente VALUES('54612345678', 'JOAO', '2000-08-22');
+INSERT INTO cliente VALUES('54612345666', 'Matheus', '1999-02-20');
+
+INSERT INTO venda VALUES(1, '32345678543', 'vendedor', '54612345678', FALSE, 30, FALSE);
+INSERT INTO venda VALUES(2, '12345678923', 'vendedor', '54612345666', FALSE, 30, TRUE);
 
 -- Deve dar erro:
 INSERT INTO farmacia VALUES(0, 'redefarma' , 'sede' , 'alguma rua2', 'algum bairro',
 300, 'bahia', '45678912345');
+INSERT INTO cliente VALUES('54612345679', 'Antonio', '2004-08-22');
+INSERT INTO cliente VALUES('54612345678', 'Maria', '2000-08-22');
+DELETE FROM medicamento WHERE id = 30;
+INSERT INTO entrega VALUES('54612345678', 1, FALSE);
